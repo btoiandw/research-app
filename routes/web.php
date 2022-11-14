f18a1167a132ed3d8fcaa1601efafc55bbb2c1f4
my git btoiandw <bbdbiw2544@gmail.com>
2022-11-14 10:50:51 +0700

    first commit with theme

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+ Route::get('/', function () {
+    return view('/login');
+});
+/* Route::get ('password/lost','ForgotPasswordController@forgotPassword'); */
+
+Auth::routes();
+Route::get ('dashboard', 'DashboardController@index');
+Route::get ('changepassword', 'UserController@changepassword');
+Route::post('updatepassword','UserController@updatePassword');
+Route::get ('profile', 'UserController@profile');
+/* Route::resource ('pages', 'PagesController'); */
+/* Route::post ('update/{user_id}', 'UserController@updateprofile');
+Route::post('changePassword/{user_id}','UserController@updatePassword')->name('changePassword'); */
+Route::get ('user/profile', 'UserController@profile');
+Route::get ('main/logout', 'MainController@logout');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
